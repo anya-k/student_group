2a3cfd33343b809b2948683c87b34d7a7a58b94f
Anya <blackqueennn@gmail.com>
2016-02-02 00:20:20 +0200

    Select headman just by member group

--- students/admin.py
+++ students/admin.py
@@ -9,8 +9,23 @@ class GroupInline(admin.TabularInline):
 
 
 class StudentAdmin(admin.ModelAdmin):
+    list_filter = ['group']
     search_fields = ['number_card']
     inlines = [GroupInline,]
 
+
+class GroupAdmin(admin.ModelAdmin):
+
+    def get_form(self, request, obj=None, **kwargs):
+        request.current_object = obj
+        return super(GroupAdmin, self).get_form(request, obj, **kwargs)
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        instance = request.current_object
+        if db_field.name == "headman":
+            kwargs["queryset"] = Student.objects.filter(group=instance.id)
+        return super(GroupAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 admin.site.register(Student, StudentAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
