8889146575b4ac9dd46865056e087db2165ed745
Anya <blackqueennn@gmail.com>
2016-02-01 23:50:15 +0200

    Admin appearance

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student, Group
+
+
+class GroupInline(admin.TabularInline):
+    model = Group
+    extra = 1
+
+
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['number_card']
+    inlines = [GroupInline,]
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Group)
--- students/models.py
+++ students/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 from django.db import models
 
@@ -7,7 +8,10 @@ class Student(models.Model):
     surname = models.CharField(max_length=40)
     first_name = models.CharField(max_length=20)
     patronymic = models.CharField(max_length=40)
-    number_card = models.IntegerField(unique=True)
+    number_card = models.PositiveIntegerField(
+        unique=True,
+        validators=[MinValueValidator(1000000000),
+                    MaxValueValidator(9999999999)])
     date_of_birth = models.DateField()
     group = models.ForeignKey('Group')
 
@@ -17,7 +21,8 @@ class Student(models.Model):
 
 class Group(models.Model):
     name = models.CharField(max_length=20)
-    headman = models.ForeignKey(Student)
+    headman = models.ForeignKey(Student, related_name='headman', null=True, blank=True)
 
     def __unicode__(self):
         return self.name
+
