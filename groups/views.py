0e5e938072ddaf03e73f8bf13503f9861ab5e463
Anya <blackqueennn@gmail.com>
2016-02-03 01:22:36 +0200

    select headman just by current student

--- groups/views.py
+++ groups/views.py
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.contrib import messages
 from groups.models import Group
 from groups.forms import GroupForm, GroupUpdateForm
+from students.models import Student
 
 
 class GroupListView(ListView):
@@ -40,6 +41,19 @@ class GroupUpdateView(UpdateView):
     form_class = GroupUpdateForm
     template_name = "students/student_form.html"
     success_url = reverse_lazy('groups:index')
+
+    def get_object(self, queryset=None):
+        object = super(GroupUpdateView, self).get_object(queryset)
+        self.request.cur_object = object
+        return object
+
+    def get_form(self, form_class=None):
+        form = super(GroupUpdateView, self).get_form(form_class)
+        if self.request.cur_object:
+            form.fields["headman"].queryset = Student.objects.filter(group=self.request.cur_object.id)
+        else:
+            form.fields["headman"].queryset = Student.objects.filter(group=None)
+        return form
 
     def get_context_data(self, **kwargs):
         context_data = super(GroupUpdateView, self).get_context_data(**kwargs)
