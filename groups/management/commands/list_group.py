7ee5f8e03a0810fbef28c514fcd63fc90b4e5a58
Anya <blackqueennn@gmail.com>
2016-02-08 01:28:46 +0200

    Add commands

---
+++ groups/management/commands/list_group.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from groups.models import Group
+from students.models import Student
+
+
+class Command(BaseCommand):
+    help = 'list of students by group'
+
+    def handle(self, *args, **options):
+        for group in Group.objects.all():
+            self.stdout.write(self.style.SUCCESS(u'Group {0}'.format(group.name)))
+            if Student.objects.filter(group=group):
+                for student in Student.objects.filter(group=group):
+                    self.stdout.write(self.style.SUCCESS(u'  {0}'.format(student.full_name())))
+            else:
+                self.stdout.write(self.style.SUCCESS(u'  No student'))
