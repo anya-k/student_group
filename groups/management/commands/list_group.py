3553b5bd107942095403c75cea9e46cac669d281
Anya <blackqueennn@gmail.com>
2016-02-08 01:38:44 +0200

    Modify compare

--- groups/management/commands/list_group.py
+++ groups/management/commands/list_group.py
@@ -9,8 +9,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for group in Group.objects.all():
             self.stdout.write(self.style.SUCCESS(u'Group {0}'.format(group.name)))
-            if Student.objects.filter(group=group):
-                for student in Student.objects.filter(group=group):
+            if Student.objects.filter(group_id=group.id):
+                for student in Student.objects.filter(group_id=group.id):
                     self.stdout.write(self.style.SUCCESS(u'  {0}'.format(student.full_name())))
             else:
                 self.stdout.write(self.style.SUCCESS(u'  No student'))
